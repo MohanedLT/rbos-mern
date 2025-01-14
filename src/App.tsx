4b2eb99925ce18237056e56fe6d5e244fc168b64
malek jarallah <malek.jarallah05@gmail.com>
2025-01-14 14:34:36 +0200

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <p>test paragraph</p>
+        <p>malek here</p>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
